6d8db60f29d182adeb03e6529c3af02891fc8725
Heidy Clase Git <kete_heidy@hotmail.com>
2018-05-11 16:12:40 -0400

    CI-001: Gruntfile updated

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,50 +3,59 @@ module.exports = function (grunt) {
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	//var config = grunt.file.readJSON('config.json');
 	var config ={};
-	config.pageContent = grunt.option('content');
-	config.srcFolder= grunt.option('src');
-	config.buildFolder=grunt.option('build');
-	config.pageTitle=grunt.option('title');
+	config.buildFolder = grunt.option('build');
+	config.appName= grunt.option('app');
+	config.pageOneName=grunt.option('one');
+	config.pageTwoName=grunt.option('two');
 
 	
 
 
-	grunt.initConfig({
-	  concat: {
-	    JS: {
-	      src: [config.srcFolder+'/js/one.js', config.srcFolder+'/js/two.js'],
-	      dest: config.buildFolder+'/scripts.js'
-	    },
-
-	    CSS: {
-	      src: [config.srcFolder+'/css/first.css', config.srcFolder+'/css/second.css',config.srcFolder+'/css/third.css'],
-	      dest: config.buildFolder+'/styles.css'
-	    }
-
-	    
-	    
-	  }
-
-  	});
+	
 
 
-
-  	grunt.registerTask('generar',function(){ 
+  	grunt.registerTask('generar1',function(){ 
 		//var config = grunt.file.readJSON('config.json');
 		
 
 		var jsonVar={prop1:true};
-grunt.file.copy(config.srcFolder+'/index.html',config.buildFolder+'/index.html',{process: function(files){
+grunt.file.copy('index.html',config.buildFolder+'/index.html',{process: function(files){
 	//console.log(content);
 
-return grunt.template.process(files,{data: {pageTitle: config.pageTitle,styles:[config.buildFolder+'/styles.css'],js:[config.buildFolder+'/scripts.js'],content: config.pageContent}});
+return grunt.template.process(files,{data: {pageTitle1: config.appName}});
+}});
+	 });
+
+  	grunt.registerTask('generar2',function(){ 
+		//var config = grunt.file.readJSON('config.json');
+		
+
+		var jsonVar={prop1:true};
+grunt.file.copy('page1.html',config.buildFolder+'/page1.html',{process: function(files){
+	//console.log(content);
+
+return grunt.template.process(files,{data: {pageTitle2: config.pageOneName}});
+}});
+
+
+grunt.registerTask('generar3',function(){ 
+		//var config = grunt.file.readJSON('config.json');
+		
+
+		var jsonVar={prop1:true};
+grunt.file.copy('page2.html',config.buildFolder+'/page2.html',{process: function(files){
+	//console.log(content);
+
+return grunt.template.process(files,{data: {pageTitle1: config.appName}});
 }});
 	 });
 
 
 
+
+
   	grunt.registerTask('build', 
-		['concat:CSS', 'concat:JS','generar']);
+		['generar1','generar2','generar3']);
 
 
 
